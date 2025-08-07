8429be0fa01d495e650b79fef67ed2d00440f4e2
S409ISDAD <129562233+S409ISDAD@users.noreply.github.com>
2025-08-07 02:34:56 +0100

    Update index.js

--- bin/index.js
+++ bin/index.js
@@ -2,7 +2,6 @@
 
 const setup = require("./lib/setup");
 const request = require('./lib/request');
-
 const cli = require('clui');
 const parser = require('node-html-parser');
 const chalk = require('chalk');
@@ -12,6 +11,7 @@ const websites = {
   tacobell: null,
   burgerking: null
 };
+
 const questionAnswers = {
   mcdonalds: {
     'R000005': 3,
@@ -46,61 +46,86 @@ const questionAnswers = {
   }
 };
 
-const sleep = async (duration) => {
-  return new Promise(resolve => setTimeout(resolve, duration));
-};
+// Helper to sleep
+const sleep = (ms) => new Promise(r => setTimeout(r, ms));
 
 const runApp = async () => {
-  const items = await setup.askQuestions();
-  const { version, code, email } = items;
-
-  if (version !== 'mcdonalds') {
-    return console.log(chalk.red('The version of this script has not yet been written'));
-  }
-
-  const website = websites[version];
-  const status = new cli.Spinner('Starting Process...');
-  status.start();
-
   try {
-    const [cn1, cn2, cn3] = code.split('-');
-    if (!cn2 || !cn3) {
-      throw new Error('Invalid Receipt Code Provided');
+    const items = await setup.askQuestions();
+    const { version, code, email } = items;
+
+    if (version !== 'mcdonalds') {
+      console.log(chalk.red('The version of this script has not yet been written'));
+      return;
     }
 
-    const res = await request.get(website);
-    const html = parser.parse(res);
-    const surveyEntryForm = html.querySelector('#surveyEntryForm');
+    const website = websites[version];
+    if (!website) {
+      console.log(chalk.red(`No website configured for version: ${version}`));
+      return;
+    }
 
-    let entryPoint = surveyEntryForm.getAttribute('action') || '';
-    entryPoint = entryPoint.replace('Index.aspx?', '');
+    const status = new cli.Spinner('Starting Process...');
+    status.start();
+
+    // Validate receipt code format
+    const [cn1, cn2, cn3] = (code || '').split('-');
+    if (!cn1 || !cn2 || !cn3) {
+      throw new Error('Invalid Receipt Code Provided. Expected format like ABC-123-456.');
+    }
+
+    // Add headers to mimic browser
+    const defaultHeaders = {
+      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)',
+      'Accept-Language': 'en-US,en;q=0.9',
+      'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8'
+    };
+
+    status.message('Getting entry point...');
+    let res = await request.get(website, { headers: defaultHeaders });
+
+    let html = parser.parse(res);
+    const entryForm = html.querySelector('#surveyEntryForm');
+    if (!entryForm) {
+      console.log(chalk.red('Failed to find the #surveyEntryForm element. Website structure may have changed or request blocked.'));
+      console.log(chalk.gray('Full HTML:'));
+      console.log(html.toString().slice(0, 1000)); // print first 1000 chars only to avoid overload
+      status.stop();
+      return;
+    }
+
+    const entryAction = entryForm.getAttribute('action');
+    if (!entryAction) {
+      throw new Error('Failed to get the action attribute from #surveyEntryForm.');
+    }
+
+    const entryPoint = entryAction.replace('Index.aspx?', '');
 
     await sleep(500);
 
-    const firstRequestData = {
+    status.message('Making first request...');
+    res = await request.post(`${website}/Index.aspx?${entryPoint}`, {
       JavascriptEnabled: '1',
       FIP: 'True',
       P: '1',
       NextButton: 'Continue'
-    };
-
-    const firstRequestResponse = await request.post(`${website}/Index.aspx?${entryPoint}`, firstRequestData);
+    }, { headers: defaultHeaders });
 
     await sleep(500);
 
-    const secondRequestData = {
+    status.message('Making second request...');
+    res = await request.post(`${website}/Index.aspx?${entryPoint}`, {
       JavascriptEnabled: '1',
       FIP: 'True',
       P: '2',
       Receipt: '1',
       NextButton: 'Next'
-    };
-
-    const secondRequestResponse = await request.post(`${website}/Index.aspx?${entryPoint}`, secondRequestData);
+    }, { headers: defaultHeaders });
 
     await sleep(500);
 
-    const submitData = {
+    status.message('Submitting receipt data...');
+    res = await request.post(`${website}/Survey.aspx?${entryPoint}`, {
       JavascriptEnabled: '1',
       FIP: 'True',
       CN1: cn1,
@@ -109,38 +134,61 @@ const runApp = async () => {
       Pound: '1',
       Pence: '99',
       NextButton: 'Start'
-    };
+    }, { headers: defaultHeaders });
 
-    const submitResponse = await request.post(`${website}/Survey.aspx?${entryPoint}`, submitData);
-    const submitHtml = parser.parse(submitResponse);
+    html = parser.parse(res);
 
-    if (submitHtml.querySelector('#BlockPage') || submitHtml.querySelector('.Error')) {
-      throw new Error('The code you tried to enter has expired.');
+    if (html.querySelector('#BlockPage') || html.querySelector('.Error')) {
+      throw new Error('The code you tried to enter has expired or is invalid.');
     }
 
+    // Loop through questions
     let questionNum = 1;
     let finished = false;
 
     do {
-      const IoNF = submitHtml.querySelector('#IoNF').getAttribute('value');
-      const PostedFNS = submitHtml.querySelector('#PostedFNS').getAttribute('value');
+      status.message(`Answering question ${questionNum}`);
 
-      const questions = [...new Set([...submitHtml.querySelectorAll('[type=checkbox],[type=radio],[type=text],textarea')].map(i => i.getAttribute('name')))];
-      const dataBuilder = { IoNF, PostedFNS };
+      html = parser.parse(res);
+
+      // Defensive checks for required hidden inputs
+      const IoNFEl = html.querySelector('#IoNF');
+      const PostedFNSEl = html.querySelector('#PostedFNS');
+
+      if (!IoNFEl || !PostedFNSEl) {
+        throw new Error('Missing required form tokens (IoNF or PostedFNS). Website may have changed.');
+      }
+
+      const IoNF = IoNFEl.getAttribute('value');
+      const PostedFNS = PostedFNSEl.getAttribute('value');
+
+      // Collect unique question names from input elements
+      const questions = [...new Set(
+        [...html.querySelectorAll('input[type=checkbox], input[type=radio], input[type=text], textarea')]
+          .map(i => i.getAttribute('name'))
+          .filter(Boolean)
+      )];
+
+      const dataBuilder = {
+        IoNF,
+        PostedFNS
+      };
 
       questions.forEach(question => {
-        if (questionAnswers[version][question]) {
-          const answer = questionAnswers[version][question] === '%%email%%' ? email : questionAnswers[version][question];
+        if (questionAnswers[version][question] !== undefined) {
+          let answer = questionAnswers[version][question];
+          if (answer === '%%email%%') answer = email;
           dataBuilder[question] = answer;
-        } else if (question.substr(0, 1) === 'R') {
-          dataBuilder[question] = 5;
+        } else if (question.startsWith('R')) {
+          dataBuilder[question] = 5;  // default rating for unknown questions starting with R
         } else {
           dataBuilder[question] = '';
         }
       });
 
-      const answerResponse = await request.post(`${website}/Survey.aspx?${entryPoint}`, dataBuilder);
+      res = await request.post(`${website}/Survey.aspx?${entryPoint}`, dataBuilder, { headers: defaultHeaders });
 
+      // If IoNF == '311' (magic number?), survey is finished
       if (IoNF === '311') {
         finished = true;
         break;
@@ -148,18 +196,18 @@ const runApp = async () => {
 
       questionNum++;
       await sleep(250);
+
     } while (questionNum < 25 && !finished);
 
     if (!finished) {
-      throw new Error('Timed out attempting to get code.');
+      throw new Error('Timed out attempting to complete the survey.');
     }
 
     status.stop();
     console.log(chalk.green(`Code generated and emailed to ${email}.`));
 
   } catch (err) {
-    status.stop();
-    console.log(chalk.red(err.message));
+    console.log(chalk.red(`Error: ${err.message}`));
   }
 };
 
